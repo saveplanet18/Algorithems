7023f1501d4c6d3a1d8121f83cf36f87e51c857f
Manjunath Kannur <yarazari10@gmail.com>
2022-08-10 10:28:34 +0530

    algorithms

---
+++ DataStructurefile/demo.py
@@ -0,0 +1,38 @@
+# class Stack:
+
+#     def __init__(self):
+
+#         self.elements = []
+    
+#     def is_empty(self):
+#         if len(self.elements) == 0:
+#             return True
+#         return False
+
+#     def push(self,ele):
+#         if len(self.elements) == 0:
+#             return self.elements.append(ele)
+#         return "stack is full"
+
+#     def peek(self):
+#         return self.elements[-1]
+
+#     def pop(self):
+#         if len(self.elements) != 0:
+#             return self.elements.pop()
+#         return "stack is empty"
+
+#     def display(self):
+#         return self.elements
+
+# if __name__ == '__main__':
+#     stack = Stack()
+#     print(stack.elements)
+#     print(stack.is_empty())
+#     mylist = [9,6,3,2,5,8,1,4,7]
+#     stack.push(mylist)
+#     print(stack.elements)
+#     print(stack.peek())
+#     print(stack.pop())
+    
+
