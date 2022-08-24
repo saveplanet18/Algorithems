e90f61a98004b4760596fbd70911a7f363d7b34c
Manjunath Kannur <yarazari10@gmail.com>
2022-08-24 13:34:03 +0530

    algorithm

---
+++ class.py
@@ -0,0 +1,29 @@
+# class Myclass:
+#     name = "manjunath"
+#     age = 33
+
+
+
+# def main():
+#     my = Myclass()
+#     print(my.name +"my age "+str(my.age))
+
+
+# if __name__ == '__main__':
+#     main()
+
+class myclass:
+    x = 0.0
+    y = 0.0
+
+    def show(self,x,y):
+        print("x is the integer,,"+str(x)+"y is another number"+str(y))
+
+def main():
+    my  = myclass()
+    my.show(9,6)
+
+
+if __name__ == '__main__':
+    main()
+        
